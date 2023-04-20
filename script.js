5afe65b4bab93de6672700efae3dfc4c8e3ed912
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-04-19 22:52:30 -0700

    working on operator function

--- script.js
+++ script.js
@@ -39,26 +39,12 @@ function divide(){
     return results;
 }
 
-function operate(num1, operator, num2){
-    switch(operator){
-        case "+":
-            return add(num1, num2);
-            break;
-        case "-":
-            return subtract(num1, num2);
-            break
-        case "*":
-            return multiply(num1, num2);
-            break;
-        case "/":
-            return divide(num1, num2);
-            break;
-    }
-}
+
+
+let currnum1 = 0, currnum2 = 0, count = 0, currop;
 var displayVal = document.getElementById("screen");
-var num = document.getElementsByClassName("num");
 var oper = document.getElementsByClassName("operator");
-var currNum1 = 90000, currNum2 = 90000;
+
 //adding eventlisteners to each button since we are getting them by class
 var numbtn = document.getElementsByClassName("num");
 for(let i = 0; i < numbtn.length; i++){
@@ -69,21 +55,60 @@ for(let i = 0; i < oper.length; i++){
     oper[i].addEventListener("click", changeDisplay);
 }
 
-function changeDisplay(evt){
-    console.log(evt.target);
-    displayVal.innerHTML = evt.target.innerHTML;
-    if(currNum1 == 90000){
-        currNum1 = evt.target.innerHTML;
-    }else{
-        currNum2 = evt.target.innerHTML;
+function operate(num1, operator, num2){
+    /*
+    switch(operator){
+        case oper[0]:
+            return(add(num1, num2));
+            break;
+        case oper[1]:
+            return(subtract(num1, num2));
+            break
+        case oper[2]:
+            return(multiply(num1, num2));
+            break;
+        case oper[4]:
+            return(divide(num1, num2));
+            break;
     }
-    if(evt.target.innerHTML){
-        alert("hey");
-    }
+    */
+    return(num1 + num2);
 }
 
-/*
-function solve(evt){
-    displayVal.innerHTML = operate(currNum1, evt.target.innerHTML, currNum2);
-    changeDisplay(displayVal);
-}*/
+
+
+
+function changeDisplay(evt){
+    //if numberbtn is pressed
+    
+    if(isNaN(evt.target.innerHTML) == false){
+        if(count == 1){
+            displayVal.innerHTML = " ";
+            count = 0;
+        }
+        displayVal.innerHTML = displayVal.innerHTML + evt.target.innerHTML;
+    
+    }else if(evt.target.innerHTML == oper[3].innerHTML){
+        let answer = operate(currnum1, currop, currnum2);
+        console.log(answer);
+        //displayVal.innerHTML = operate(currnum1, currop, currnum2);
+        displayVal.innerHTML = answer;
+
+    }else{
+        //this function doesnt go because NaN goes first 
+        //if operatorbtn is pressed
+        console.log("number");
+        if(currnum1 == 0){
+            currnum1 = evt.target.innerHTML;
+            console.log(currnum1);
+        }else{
+            currnum2 = evt.target.innerHTML;
+            console.log(currnum2);
+        }
+        
+        displayVal.innerHTML = evt.target.innerHTML;
+        count++;
+    }
+    
+
+}
